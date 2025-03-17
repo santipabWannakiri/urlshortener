6f509a3cf07c8a1d8ccfdbab4e121daa7469caa3
Nokia3310 <santipab1991@mail.com>
2025-03-17 17:53:49 -0400

    initail project

---
+++ src/test/java/com/shortened/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
