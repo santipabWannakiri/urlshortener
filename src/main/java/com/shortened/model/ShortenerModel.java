bbeb32030374227973ab55e984c900dc83c72aff
Nokia3310 <santipab1991@mail.com>
2025-03-21 23:33:20 -0400

    add generic response, add shortenerModel, add requestModel

--- src/main/java/com/shortened/model/ShortenerModel.java
+++ src/main/java/com/shortened/model/ShortenerModel.java
@@ -9,10 +9,36 @@ import lombok.Data;
 public class ShortenerModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String original_url;
     private String shortened_url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginal_url() {
+        return original_url;
+    }
+
+    public void setOriginal_url(String original_url) {
+        this.original_url = original_url;
+    }
+
+    public String getShortened_url() {
+        return shortened_url;
+    }
+
+    public void setShortened_url(String shortened_url) {
+        this.shortened_url = shortened_url;
+    }
+
+    public ShortenerModel() {
+    }
 
     public ShortenerModel(Long id, String original_url, String shortened_url) {
         this.id = id;
