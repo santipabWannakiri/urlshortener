c9e81bf30cee85cefd3823ff0dc68d3427d9e95d
Nokia3310 <santipab1991@mail.com>
2025-03-29 11:52:31 -0400

    work in progress before rebase

--- src/main/java/com/shortened/UrlshortenerApplication.java
+++ src/main/java/com/shortened/UrlshortenerApplication.java
@@ -1,13 +1,17 @@
 package com.shortened;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlshortenerApplication {
+	private static final Logger logger = LogManager.getLogger(UrlshortenerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortenerApplication.class, args);
+		logger.info("Application started successfully.");
 	}
 
 }
